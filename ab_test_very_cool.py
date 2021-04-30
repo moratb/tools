45b914786142a8a662a814228731f0ef8e76c778
Marat Gudkov <12eyedfox@gmail.com>
2021-04-30 18:26:15 +0300

    Update ab_test_very_cool.py

--- ab_test_very_cool.py
+++ ab_test_very_cool.py
@@ -16,20 +16,3 @@ for name, val in vals:
     else:
         is_success = False
     print(name, is_success, 'a=',  val,  'b=',  val*k , 'diff=', (Q1*arpu_std/N**0.5))
-    
-    
-    
-## Хиквадрат
-for i in static_ab_stat_ARPU['platform'].unique():
-    tmp_dat = static_ab_stat_ARPU[static_ab_stat_ARPU['platform']==i]
-    obs = np.array([
-                [                 
-                 int(tmp_dat[tmp_dat['test_group']=='test_A']['uid']) - int(tmp_dat[tmp_dat['test_group']=='test_A']['payers']),
-                 int(tmp_dat[tmp_dat['test_group']=='test_A']['payers']),
-                ],
-                [
-                 int(tmp_dat[tmp_dat['test_group']=='test_G']['uid']) - int(tmp_dat[tmp_dat['test_group']=='test_G']['payers']),
-                 int(tmp_dat[tmp_dat['test_group']=='test_G']['payers'])
-                ]])
-    chi2, p, dof, expected = scipy.stats.chi2_contingency(obs)
-    print(i ,obs, p)
